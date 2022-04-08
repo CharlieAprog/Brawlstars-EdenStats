b5939220dd5e43c9324da3d5ab62b655318063bd
CharlieAprog <charlie.albietz.uni@gmail.com>
2022-04-08 13:28:01 +0200

    quick purge

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-
-
-if __name__ == '__main__':
-    if 'src' not in os.getcwd():
-        os.chdir('src')
--- src/players.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-class Player():
-    def __init__(self, name, entry_week, current_team) -> None:
-        self.name = name
-        self.enty_week = entry_week
-        self.ratio = 0
-        self.current_team = current_team
